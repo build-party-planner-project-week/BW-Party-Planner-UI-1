2bed689c2e200074631c7c4851921a2878156d2c
Desiree Morris <desiquinncompany@gmail.com>
2019-06-27 10:18:28 -0400

    formatted the functionality of the drop down menu via javascript

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,5 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
-
+const dropDown = document.querySelector(".dropDown");
 // console.log(hamburger);
+// console.log(dropDown);
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("change");
+    dropDown.classList.toggle("hideDropDown");
+});
 
-hamburger.addEventListener('click', () => hamburger.classList.toggle("change"));
